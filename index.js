c1225568ed7308530f68a0764f77bed425e6484b
CyrilAdolf <cyril.adolf@gmail.com>
2020-11-09 16:24:36 +0100

    add dotenv package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
 app.use(cors());
 
@@ -460,6 +460,6 @@ app.get("/", (req, res) => {
 //     console.log("Server started");
 //   });
 // }
-app.listen(process.env.PORT || 3200, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
